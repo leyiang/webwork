f076dbf7a3b15a76c980b73bc496f2e542737e5d
Yiang <yiangforwork@gmail.com>
2022-07-22 03:39:28 +0800

    add. compile

---
+++ compile.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+function getComponent( name ) {
+    return `
+        <a href="/${name}" class="project-item">
+            <span>${ name }</span>
+        </a>
+    `;
+}
+
+const folders = fs.readdirSync("./src")
+
+const raw = folders.map( folder => {
+    return getComponent(folder);
+});
+
+
+fs.readFile("index.html", "utf-8", function(err, data) {
+    if( err ) {
+        return console.log( err );
+    }
+
+    const result = data.replace("<!--project-->", raw.join('') );
+
+    fs.writeFile("./src/index.html", result, "utf-8", function(err) {
+        if( err ) {
+            console.log( err );
+        }
+    });
+});
